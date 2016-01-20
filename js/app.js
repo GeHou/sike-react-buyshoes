2372872d637e922fddae14f3d3c1a0df9de4fccb
GeHou <8008086@gmail.com>
2016-01-20 18:13:15 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 window.onload = function() {
   console.log("page loaded");
   makeCartScrollNicely();
+  toggleSidebar();
 }
 
 function makeCartScrollNicely() {
   var cart = document.querySelector(".cart");
   Ps.initialize(cart);
+}
+
+function toggleSidebar () {
+  var $toggle = document.querySelector(".site__right-sidebar-toggle");
+  $toggle.addEventListener("click",function() {
+    document.body.classList.toggle("js-show-right-sidebar");
+  });
 }
